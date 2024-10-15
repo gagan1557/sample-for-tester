0fc12a0d6c56608fd90c5d5816e11c39b5239096
gagan1557 <gagan1557.be22@chitkara.edu.in>
2024-10-15 11:49:59 +0530

    changes

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,7 +3,8 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "http://localhost:8000/api",
+  // baseURL: "http://localhost:8000/api",
+  baseURL: "https://rdg-real-estate-backend.onrender.com",
 });
 
 export const getAllProperties = async () => {
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({origin: 'http://localhost:5173/', credentials: true}))
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`);
